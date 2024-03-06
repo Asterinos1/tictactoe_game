ae6b5941f63adf710b40d643d9e1601944330207
Asterinos1 <karalhs.as@gmail.com>
2024-03-06 23:32:59 +0200

    Ola kala

--- src/View/HallOfFame.java
+++ src/View/HallOfFame.java
@@ -1,47 +1,52 @@
 package View;
 
 import java.awt.Color;
-import java.awt.GridLayout;
+import java.awt.Font;
 import java.util.List;
-
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
 import Model.Player;
 import Model.PlayerRoster;
 
 public class HallOfFame extends JPanel {
-    private JLabel ScreenTitle = new JLabel("Hall of fame");
+    private JLabel screenTitle = new JLabel("Hall of Fame");
     private PlayerRoster playerRoster;
     private JPanel playerListPanel;
-    
-    public HallOfFame(PlayerRoster pr){
-        this.playerRoster=pr;
+
+    public HallOfFame(PlayerRoster pr) {
+        this.playerRoster = pr;
         setupHallOfFame();
     }
 
     private void setupHallOfFame() {
-        this.setBackground(Color.white);
+        this.setBackground(Color.WHITE);
         this.setBounds(300, 100, 600, 700);
-        this.add(ScreenTitle);
+        this.screenTitle.setFont(new Font("Arial", Font.BOLD, 40));
+        this.add(screenTitle);
+
+        playerListPanel = new JPanel();
+        playerListPanel.setBackground(Color.WHITE);
+        playerListPanel.setLayout(new BoxLayout(playerListPanel, BoxLayout.Y_AXIS)); // Vertical layout for player names and scores
+        this.add(playerListPanel);
+
+        displayPlayers(); // Display players immediately
     }
 
-    public void updatePlayerRoster(PlayerRoster pr){
-        this.playerRoster=pr;
+    private void displayPlayers() {
+        List<Player> players = playerRoster.findHallOfFame(); // Get list of players from PlayerRoster
+        int index = 1;
+        for (Player player : players) {
+            JLabel playerLabel = new JLabel(index + player.getName() + " - Score: " + player.getScore());
+            playerLabel.setFont(new Font("Arial", Font.BOLD, 16)); // Set a larger font size, you can adjust the font and size as needed
+            playerListPanel.add(playerLabel);
+            index++;
+        }
     }
 
-    private void updatePlayerList() {
-        List<Player> topPlayers = playerRoster.findHallOfFame();
-        if (playerListPanel != null) {
-            remove(playerListPanel); // Clear previous player list
-        }
-        playerListPanel = new JPanel(new GridLayout(topPlayers.size(), 1)); // Vertical layout for player names
-        for (Player player : topPlayers) {
-            JLabel playerNameLabel = new JLabel(player.getName());
-            playerListPanel.add(playerNameLabel);
-        }
-        add(playerListPanel);
-        revalidate(); // Refresh the layout
-        repaint(); // Repaint the panel
+    public void updatePlayerRoster(PlayerRoster pr) {
+        this.playerRoster = pr;
+        playerListPanel.removeAll(); // Clear previous players
+        displayPlayers(); // Display updated list
     }
 }
