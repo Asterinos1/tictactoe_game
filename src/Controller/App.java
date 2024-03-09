bd9b96f8311743d7562902c4c39add43fa89c583
Asterinos1 <karalhs.as@gmail.com>
2024-03-09 12:37:41 +0200

    Removed unused stuff

--- src/Controller/App.java
+++ src/Controller/App.java
@@ -3,25 +3,6 @@ import View.MainWindow;
 
 public class App {
     public static void main(String[] args) throws InterruptedException {
-        //pr.lodDataFromFile();
         MainWindow mw = new MainWindow();
-        
-        /* pr.addNewPlayer("Asterinos");
-        pr.addNewPlayer("John");
-        pr.addNewPlayer("George");
-        pr.addNewPlayer("Eleni");
-        pr.printCurrentRoaster();
-
-        pr.createNewGamerecord("Asterinos", "John", 0);    
-        pr.createNewGamerecord("Asterinos", "Eleni",1);       
-        pr.createNewGamerecord("John", "Asterinos",1);      
-        pr.createNewGamerecord("George", "Eleni",2);
-        pr.createNewGamerecord("Asterinos", "George",0);
-
-        pr.printAllGames();
-        pr.printAllPlayersStats();
-
-        pr.saveDataToFile();
-        pr.findHallOfFame();*/
     }
 }
